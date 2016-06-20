7b90b54a2081e43ebecd6b9d2bb034ab22860acb
Caius Durling <dev@caius.name>
2016-06-20 22:01:31 +0100

    Read stdin when asked to read "-"

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ use std::io::Write;
 use std::path::Path;
 use std::path::PathBuf;
 
+fn copy_stdio() {
+  io::copy(&mut io::stdin(), &mut io::stdout()).unwrap();
+}
+
 fn copy_file(path: PathBuf) {
   let mut file = File::open(path).unwrap();
   io::copy(&mut file, &mut io::stdout()).unwrap();
@@ -15,14 +19,18 @@ fn main() {
   let argv = env::args().skip(1);
 
   if argv.len() == 0 {
-    io::copy(&mut io::stdin(), &mut io::stdout()).unwrap();
+    copy_stdio();
     return
   }
 
   for path in argv {
-    match Path::new(&path).canonicalize() {
-      Err(_) => writeln!(io::stderr(), "cat: {}: No such file or directory", path).unwrap(),
-      Ok(filepath) => copy_file(filepath),
+    if path == "-" {
+      copy_stdio();
+    } else {
+      match Path::new(&path).canonicalize() {
+        Err(_) => writeln!(io::stderr(), "cat: {}: No such file or directory", path).unwrap(),
+        Ok(filepath) => copy_file(filepath),
+      }
     }
   }
 
