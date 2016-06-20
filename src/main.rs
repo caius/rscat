99d40cd98fc20a3db50f4df575db89a2549981d0
Caius Durling <dev@caius.name>
2016-06-20 21:24:32 +0100

    Handle reading files that exist

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
+use std::fs::File;
 use std::io;
+use std::path::Path;
 
 fn main() {
 
@@ -7,5 +9,13 @@ fn main() {
 
   if argv.len() == 0 {
     io::copy(&mut io::stdin(), &mut io::stdout()).unwrap();
+  } else {
+    let handlers = argv
+      .map (|path| Path::new(&path).canonicalize().unwrap() )
+      .map (|path| File::open(&path).unwrap() );
+
+    for mut file in handlers {
+      io::copy(&mut file, &mut io::stdout()).unwrap();
+    }
   }
 }
